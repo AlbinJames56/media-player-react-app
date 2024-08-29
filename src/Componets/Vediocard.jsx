a425f953efce360041af518645bc2eedcef8559f
AlbinJames56 <albinjames56@gmail.com>
2024-08-29 14:32:43 +0530

    serrvel url added

--- src/Componets/Vediocard.jsx
+++ src/Componets/Vediocard.jsx
@@ -3,7 +3,7 @@ import { Button, Card, Modal } from "react-bootstrap";
 import { useState } from "react";
 import { addVideoHistoryAPI, deleteVideoAPI } from "../../services/allAPI";
 
-function Vediocard({ video, setDeleteVideoResponse ,insideCategory}) {
+function Vediocard({ video, setDeleteVideoResponse, insideCategory }) {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
@@ -22,28 +22,38 @@ function Vediocard({ video, setDeleteVideoResponse ,insideCategory}) {
     let videoHistory = { name, link, timeStamp };
     await addVideoHistoryAPI(videoHistory);
   };
-  const removeVideo=async(id)=>{
-    await deleteVideoAPI(id)
-    setDeleteVideoResponse(true) 
-  }
+  const removeVideo = async (id) => {
+    await deleteVideoAPI(id);
+    setDeleteVideoResponse(true);
+  };
 
-  const DragStarted=(e,id)=>{
+  const DragStarted = (e, id) => {
     console.log(id);
-  e.dataTransfer.setData("VideoId", id);
-  
-  }
+    e.dataTransfer.setData("VideoId", id);
+  };
   return (
     <>
-      <Card style={{ width: "15rem" }} draggable onDragStart={e=>DragStarted(e,video?.id)}>
-        <Card.Img variant="top" width={"100%"} style={{aspectRatio:"1.7/1"}} src={video.url} onClick={handleShow} />
+      <Card
+        style={{ width: "15rem" }}
+        draggable
+        onDragStart={(e) => DragStarted(e, video?.id)}
+      >
+        <Card.Img
+          variant="top"
+          width={"100%"}
+          style={{ aspectRatio: "1.7/1" }}
+          src={video.url}
+          onClick={handleShow}
+        />
         <Card.Body>
           <Card.Title className="d-flex justify-content-between align-items-center">
             {video?.name}
           </Card.Title>
-            {insideCategory?null:
-          <Button variant="primary" onClick={()=> removeVideo(video?.id)}>
-            <i className="fa-solid fa-trash text-white"></i>
-          </Button>}
+          {insideCategory ? null : (
+            <Button variant="primary" onClick={() => removeVideo(video?.id)}>
+              <i className="fa-solid fa-trash text-white"></i>
+            </Button>
+          )}
         </Card.Body>
       </Card>
       <Modal show={show} onHide={handleClose} lg>
